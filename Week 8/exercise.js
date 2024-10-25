d329b6cdd201869e9f996dc5640fb44365079be8
Robert Johnson <rbrucej@gmail.com>
2024-10-25 00:24:53 -0400

    Week 8

---
+++ Week 8/exercise.js
@@ -0,0 +1,47 @@
+/*
+ * This exercise is to show more examples of JavaScript basics.
+ * Assignment: fill in the function at the end of each section and get the tests to pass.
+ */
+
+var display = document.getElementById("display");
+
+/*************************** Conditionals *************************/
+
+
+// Takes in three values and returns the maximum value
+// Example: maxOfThree(3, 6, 4) -> returns 4
+//          maxOfThree(9, 2, 5) -> returns 9
+function maxOfThree(param1, param2, param3)
+{
+}
+
+/******************************* Loops *****************************/
+
+// Takes in one value and returns how many times it can be divided by 2 until the value is less than 1
+// Example: binaryMagnitute(10) -> returns 4 (10 -> 5 -> 2.5 -> 1.25 -> 0.625)
+//          binaryMagnitute(24) -> returns 5 (24 -> 12 -> 6 -> 3 -> 1.5 -> 0.75)
+//          binaryMagnitute(16) -> returns 5 (16 -> 8 -> 4 -> 2 -> 1 -> 0.5)
+function binaryMagnitute(param)
+{
+}
+
+/******************************* Arrays *****************************/
+
+// Takes in an array and returns the value in the middle of the array, rounded down.
+// Hint: Math.floor(value) will round a number down.
+// Example: middle([1, 3, 5]) -> returns 3
+//          middle([6, "hi", 2, "bye"]) -> returns "hi"
+function middle(array)
+{
+}
+
+/****************************** Objects *****************************/
+
+// Takes in an object with the properties of an address and returns a string with the properties formated into a single value.
+// The input object will have the properties name, street, state, and zipcode.
+// Example: formatAddress({name: "Smith", street: "104 Elm", state: "WY", zipcode: "12345"}) -> returns "Smith, 104 Elm, WY, 12345"
+//          formatAddress({name: "Jones", street: "56 State", state: "HI", zipcode: "67890"}) -> returns "Jones, 56 State, HI, 67890"
+function formatAddress(address)
+{
+}
+
