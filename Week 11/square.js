5b8f17b8b3bce9366bfd11729c1c3b960ff39389
Robert Johnson <rbrucej@gmail.com>
2024-11-15 00:07:13 -0500

    Squares

---
+++ Week 11/square.js
@@ -0,0 +1,42 @@
+/**
+ * Assignment: add code that will make a div element for each item in the squares array.
+ * The element should have the their background color equal to the color value in the item, and their text equal to the icon value.
+ * If the background color is black the text color needs to be white.
+ * An example of what the final result should look like is included in the files. 
+ */
+
+
+var squares = [
+    {
+        color: "#ff0000",
+        icon: "R"
+    },
+    {
+        color: "#00ff00",
+        icon: "G"
+    },
+    {
+        color: "#0000ff",
+        icon: "B"
+    },
+    {
+        color: "#ff00ff",
+        icon: "M"
+    },
+    {
+        color: "#ffff00",
+        icon: "Y"
+    },
+    {
+        color: "#00ffff",
+        icon: "C"
+    },
+    {
+        color: "#ffffff",
+        icon: "W"
+    },
+    {
+        color: "#000000",
+        icon: "B"
+    }
+];
