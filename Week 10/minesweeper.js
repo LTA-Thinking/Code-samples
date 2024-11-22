8e72c8046682949f7b66f3e23d88adef123c9914
Robert Johnson <rbrucej@gmail.com>
2024-11-22 10:05:24 -0500

    End of week 11

--- Week 10/minesweeper.js
+++ Week 10/minesweeper.js
@@ -1,88 +1,78 @@
 
 var map;
+var tiles;
 var clickedCells = 0;
 var maxX = 10;
 var maxY = 10;
 var numberOfMines = 20;
 var gameOver = false;
 
-function addMine()
-{
-    var xCord = Math.floor(Math.random()*maxX);
-    var yCord = Math.floor(Math.random()*maxY);
+function addMine() {
+    var xCord = Math.floor(Math.random() * maxX);
+    var yCord = Math.floor(Math.random() * maxY);
 
-    if (map[xCord][yCord] === -1)
-    {
+    if (map[xCord][yCord] === -1) {
         return addMine();
     }
 
     map[xCord][yCord] = -1;
 }
 
-function incrementCell(xCord, yCord)
-{
+function incrementCell(xCord, yCord) {
     if (xCord < 0 || xCord >= maxX ||
         yCord < 0 || yCord >= maxY ||
-        map[xCord][yCord] === -1)
-    {
+        map[xCord][yCord] === -1) {
         return;
     }
 
     map[xCord][yCord]++;
 }
 
-function createMap()
-{
+function createMap() {
     map = [];
 
-    for (var x = 0; x < maxX; x++)
-    {
+    for (var x = 0; x < maxX; x++) {
         map[x] = [];
-        for (var y = 0; y < maxY; y++)
-        {
+        for (var y = 0; y < maxY; y++) {
             map[x][y] = 0;
         }
     }
 
-    for (var mine = 0; mine < numberOfMines; mine++)
-    {
+    for (var mine = 0; mine < numberOfMines; mine++) {
         addMine();
     }
 
-    for (var x = 0; x < maxX; x++)
-    {
-        for (var y = 0; y < maxY; y++)
-        {
-            if (map[x][y] === -1)
-            {
-                incrementCell(x-1, y-1);
-                incrementCell(x-1, y);
-                incrementCell(x-1, y+1);
-                incrementCell(x, y-1);
-                incrementCell(x, y+1);
-                incrementCell(x+1, y-1);
-                incrementCell(x+1, y);
-                incrementCell(x+1, y+1);
+    for (var x = 0; x < maxX; x++) {
+        for (var y = 0; y < maxY; y++) {
+            if (map[x][y] === -1) {
+                incrementCell(x - 1, y - 1);
+                incrementCell(x - 1, y);
+                incrementCell(x - 1, y + 1);
+                incrementCell(x, y - 1);
+                incrementCell(x, y + 1);
+                incrementCell(x + 1, y - 1);
+                incrementCell(x + 1, y);
+                incrementCell(x + 1, y + 1);
             }
         }
     }
 }
 
-function createGrid()
-{
+function createGrid() {
     var table = document.createElement("table");
+    tiles = [];
 
-    for (var x = 0; x < maxX; x++)
-    {
+    for (var x = 0; x < maxX; x++) {
         var row = document.createElement("tr");
+        tiles[x] = [];
 
-        for (var y = 0; y < maxY; y++)
-        {
+        for (var y = 0; y < maxY; y++) {
             var cell = document.createElement("td");
             cell.classList.add("cell");
             cell.addEventListener("mousedown", clickFactory(x, y));
             // cell.innerText = map[x][y];
             row.appendChild(cell);
+            tiles[x][y] = cell;
         }
         table.appendChild(row);
     }
@@ -91,37 +81,68 @@ function createGrid()
     content.appendChild(table);
 }
 
-function clickFactory(x, y)
-{
+function clickFactory(x, y) {
     return (event) => onClick(event, x, y);
 }
 
-function onClick(event, x, y)
-{
-    if (gameOver === true || event.target.disabled === true)
-    {
+function onClick(event, x, y) {
+    if (x < 0 || x >= maxX ||
+        y < 0 || y >= maxY) {
         return;
     }
 
-    event.target.classList.add("disabled");
-    event.target.disabled = true;
+    var tile = tiles[x][y];
 
-    if (map[x][y] === -1)
-    {
-        alert("Lose :(");
-        gameOver = true;
+    if (gameOver === true || tile.disabled === true) {
         return;
     }
 
-    event.target.innerText = map[x][y];
-    clickedCells++;
+    if (event.button === 0) {
+        tile.classList.add("disabled");
+        tile.disabled = true;
 
-    if (clickedCells === maxX * maxY - numberOfMines) 
+        if (map[x][y] === -1) {
+            alert("Lose :(");
+            for (var xCord = 0; xCord < maxX; xCord++)
+            {
+                for (var yCord = 0; yCord < maxY; yCord++)
+                {
+                    tiles[xCord][yCord].innerText = map[xCord][yCord];
+                }
+            }
+            gameOver = true;
+            return;
+        }
+
+        tile.innerText = map[x][y];
+        clickedCells++;
+
+        if (clickedCells === maxX * maxY - numberOfMines) {
+            alert("Win!");
+            gameOver = true;
+            return;
+        }
+
+        if (map[x][y] === 0) {
+            onClick(event, x-1, y-1);
+            onClick(event, x-1, y);
+            onClick(event, x-1, y+1);
+            onClick(event, x, y-1);
+            onClick(event, x, y+1);
+            onClick(event, x+1, y-1);
+            onClick(event, x+1, y);
+            onClick(event, x+1, y+1);
+        }
+    }
+    else if(event.button === 2)
     {
-        alert("Win!");
-        gameOver = true;
+        tile.classList.toggle("flagged");
     }
 }
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
 
 createMap();
 createGrid();
