390046def7a711a3c37bcb0422c662b34a7a73f2
Robert Johnson <rbrucej@gmail.com>
2024-11-22 11:28:01 -0500

    work from class

--- Week 12/problems.js
+++ Week 12/problems.js
@@ -3,3 +3,46 @@
  * Assignment: Do Euler Problem #1, Multiples of 3 or 5. You do not have to make an account, but you can if you want to see if your answer is correct.
  */
 
+function problem2()
+{
+    var max = 4000000;
+    var first = 1;
+    var second = 2;
+    var sum = 2;
+
+    while (second < max)
+    {
+        var next = first + second;
+        first = second;
+        second = next;
+        if (next < max && next % 2 === 0)
+        {
+            sum += next;
+        }
+    }
+    
+    displayAnswer(2, sum);
+}
+
+function problem3()
+{
+    var number = 600851475143;
+    var current = 2;
+
+    while (current < number)
+    {
+        if (number % current === 0)
+        {
+            number = number / current;
+        }
+        else
+        {
+            current++;
+        }
+    }
+
+    displayAnswer(3, number);
+}
+
+problem2();
+problem3();
---
+++ Week 12/working.js
@@ -0,0 +1,34 @@
+
+var test = 4;
+console.log(test);
+
+test = test + 3;
+
+var group = [2, 4, 7, 5];
+console.log(group[0]);
+
+var value = 3;
+if (value > 4)
+{
+    // do thing
+}
+
+while (value < 12)
+{
+    value = Math.random()*20;
+}
+
+function doAThing(thing)
+{
+
+}
+
+
+doAThing(value)
+
+
+
+
+
+
+
